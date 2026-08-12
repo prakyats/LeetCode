c1663da428b2fd9beaa61d7c28e69faff65c937b
Prakyat Shetty <shettyprakyat15@gmail.com>
2026-08-12 11:29:30 +0530

    Time: 0 ms (100%), Space: 10.1 MB (71.04%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,8 +4,7 @@ public:
         int m=matrix.size();
 
         for(int i =0;i<m;i++){
-            for(int j=0;j<m;j++){
-                if(j>i)
+            for(int j=i+1;j<m;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
