4a267f6a827a8f464ee5a39cc207bcab691f5023
Prakyat Shetty <shettyprakyat15@gmail.com>
2026-08-14 11:28:23 +0530

    Time: 0 ms (100%), Space: 9.1 MB (98.89%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -2,20 +2,32 @@ class Solution {
   public:
     bool isIsomorphic(string &s1, string &s2) {
         // code here
-        unordered_map<char,char> fwd;
-        unordered_map<char,char> bwd;
+        int m1[127]={0},m2[127]={0};
         for(int i =0;i<s1.size();i++){
-            if(fwd.find(s1[i])!=fwd.end() || bwd.find(s2[i])!=bwd.end()){
-                if(fwd[s1[i]]!=s2[i] && bwd[s2[i]]!=s1[i])
-                return false;
-            }
-            else if (fwd.find(s1[i])==fwd.end() && bwd.find(s2[i])==bwd.end()){
-                fwd[s1[i]]=s2[i];
-                bwd[s2[i]]=s1[i];
-            }
-            else
+            if(m1[s1[i]]!=m2[s2[i]])
             return false;
+            m1[s1[i]]=i+1;
+            m2[s2[i]]=i+1;
         }
         return true;
     }
 };
+
+
+
+
+        // unordered_map<char,char> fwd;
+        // unordered_map<char,char> bwd;
+        // for(int i =0;i<s1.size();i++){
+        //     if(fwd.find(s1[i])!=fwd.end() || bwd.find(s2[i])!=bwd.end()){
+        //         if(fwd[s1[i]]!=s2[i] && bwd[s2[i]]!=s1[i])
+        //         return false;
+        //     }
+        //     else if (fwd.find(s1[i])==fwd.end() && bwd.find(s2[i])==bwd.end()){
+        //         fwd[s1[i]]=s2[i];
+        //         bwd[s2[i]]=s1[i];
+        //     }
+        //     else
+        //     return false;
+        // }
+        // return true;
