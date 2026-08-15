6cf6f74692828efde8b408c19ba62b6941231582
Prakyat Shetty <shettyprakyat15@gmail.com>
2026-08-15 17:50:25 +0530

    Time: 0 ms (100%), Space: 10.9 MB (69.07%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -5,11 +5,10 @@ public:
         for (auto it : s)
             mp[it]++;
         string ans = "";
-        vector<pair<char, int>> mp1(mp.begin(), mp.end());
-        sort(mp1.begin(), mp1.end(),
+        vector<pair<char, int>> v(mp.begin(), mp.end());
+        sort(v.begin(), v.end(),
              [](const auto& a, const auto& b) { return a.second > b.second; });
-
-        for (auto it : mp1) {
+        for (auto it : v) {
             while (it.second--) {
                 ans += it.first;
             }
