aad91cef156dbb2899520c28313ef80e6eb4a6de
Prakyat Shetty <shettyprakyat15@gmail.com>
2026-08-25 22:29:58 +0530

    Time: 1 ms (19.99%), Space: 9.4 MB (15.38%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        map<char, int> mp;
+
+        for (auto it : t) {
+            mp[it]++;
+        }
+
+        for (auto it : s) {
+            mp[it]--;
+        }
+
+        for (auto it : mp) {
+            if (it.second > 0)
+                return it.first;
+        }
+
+        return ' ';
+    }
+};
